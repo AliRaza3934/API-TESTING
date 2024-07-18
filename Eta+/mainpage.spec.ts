56307181febf3448dbe5150577bb65f0593182d9
AliRaza <ali.raza@etaplus.energy>
2024-07-18 13:26:36 +0200

    Automate First login Test case with correct credentials Testcase no A2

--- Eta+/mainpage.spec.ts
+++ Eta+/mainpage.spec.ts
@@ -1,13 +1,21 @@
 import { test, expect } from '@playwright/test';
 import{chromium,Page,Browser} from '@playwright/test';
-import { IntEnviorment } from './pages/LoginPage';
+import { IntEnviorment } from './pages/loginPage';
 
 //let loginFunc = new Login();
 
-
+//A2 Fist Login Test case
 test('Int Enviorment', async ({ page }) => {
-  const loginPage = new IntEnviorment(page);
-  test.setTimeout(30000);
+  
+  const intenviorment = new IntEnviorment(page);
+  await page.goto('https://int.eta-plus.com/#!/startpage/login');
+  await intenviorment.usernameField();
+  await intenviorment.passwordField();
+  await intenviorment.signinButton();
+ 
+  await page.waitForTimeout(50000)
+  
+  //test.setTimeout(30000);
  // await  loginFunc.loginUser("ETA+PMO","Mav34733");
   
 // Expect a title "to contain" a substring.
--- Eta+/pages/loginPage.ts
+++ Eta+/pages/loginPage.ts
@@ -3,11 +3,26 @@ import{Page} from '@playwright/test'
 
 export class IntEnviorment{
 
-userNameField   = () => this.page.locator("//input[@id='username']")
-passwordField   = () => this.page.locator("//input[@placeholder='Passwort']")
-siginButton     = () => this.page.locator("//span[@class='p-button-label']")
+public async usernameField(){
+
+    await this.page.locator("#username").fill("ETA+ PMO");
+}
+public async passwordField(){
+    await this.page.locator("//input[@placeholder='Passwort']").fill("Mav34733");
+}
+public async signinButton(){
+
+    await this.page.locator("//span[@class='p-button-label']").click()
+}
 
 
-public async login(){
-    await this.page.goto('https://int.eta-plus.com/#!/startpage/login');
-    await this.
+
+    
+
+//intenviorment: IntEnviorment;
+constructor(public page : Page){
+    this.page = page;
+    //this.intenviorment = new IntEnviorment(this.page);
+}
+
+}
