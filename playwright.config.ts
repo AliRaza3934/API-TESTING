f07627fa03b81d59990b2ef5a14e5722484466e5
AliRaza <ali.raza@etaplus.energy>
2024-07-20 16:32:33 +0200

    adding video and audio

--- playwright.config.ts
+++ playwright.config.ts
@@ -88,4 +88,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   //webServer: {
    // command: 'npm run start',
-   // url: 'http://
+   // url: 'http://127.0.0.1:3000',
+    //reuseExistingServer: !process.env.CI,  // Ensure this line is complete
+  //},
+});
